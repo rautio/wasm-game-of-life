5e4f3aa0f020162b9e165c3fbbddd9e4aae1cac0
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2024-02-15 20:08:00 -0800

    it works

--- www/index.js
+++ www/index.js
@@ -1,13 +1,74 @@
-import { Universe } from "wasm-game-of-life";
+import { Universe, Cell } from "wasm-game-of-life";
+import { memory } from 'wasm-game-of-life/wasm_game_of_life_bg';
 
-const pre = document.getElementById('game-of-life-canvas');
+const CELL_SIZE = 10; // px
+const GRID_COLOR = '#cccccc';
+const DEAD_COLOR = '#ffffff';
+const ALIVE_COLOR = '#000000';
+
 const universe = Universe.new();
+const width = universe.width();
+const height = universe.height();
+
+const canvas = document.getElementById('game-of-life-canvas');
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+const ctx = canvas.getContext('2d');
 
 const renderLoop = () => {
-  pre.textContent = universe.render();
   universe.tick();
+
+  drawGrid();
+  drawCells();
 
   requestAnimationFrame(renderLoop);
 }
 
+const drawGrid = () => {
+  ctx.beginPath();
+  ctx.strokeStyle = GRID_COLOR;
+
+  // Vertical lines
+  for (let i = 0; i <= width; i++) {
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+  // Horizontal lines
+  for (let i = 0; i <= height; i++) {
+    ctx.moveTo(0, i * (CELL_SIZE + 1) + 1);
+    ctx.moveTo((CELL_SIZE + 1) * width + 1, i * (CELL_SIZE + 1) + 1);
+  }
+
+  ctx.stroke();
+}
+
+const getIndex = (row, column) => {
+  return row * width + column;
+}
+
+const drawCells = () => {
+  const cellsPtr = universe.cells();
+  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+  ctx.beginPath();
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+
+      ctx.fillStyle = cells[idx] === Cell.Dead ? DEAD_COLOR : ALIVE_COLOR;
+      ctx.fillRect(
+        col * (CELL_SIZE + 1 ) + 1,
+        row * (CELL_SIZE + 1 ) + 1,
+        CELL_SIZE,
+        CELL_SIZE,
+      );
+    }
+  }
+  ctx.stroke();
+}
+
+// Init
+drawGrid();
+drawCells();
 requestAnimationFrame(renderLoop);
